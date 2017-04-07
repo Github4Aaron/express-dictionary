add764a873da1cb75bcc3ef61aa9793e2079813d
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-07 14:28:35 -0400

    used process.exit and callback prompts to collect name and sayings

--- promptPerson.js
+++ promptPerson.js
@@ -13,6 +13,19 @@ rl.question("What is the name of a real person?", function(answer) {
    rl.prompt();
 
    rl.on('line', function(saying){
-    console.log(saying.trim());
+       realPerson.sayings.push(saying.trim());
+
+       if (saying.toLowerCase().trim() === 'exit') {
+           rl.close();
+       } else {
+        rl.setPrompt(`What else would ${realPerson.name} say? ('exit' to leave)`); //when we collect teh saying, the prompt is reset.
+    
+        rl.prompt();
+       }
    });
 });
+
+rl.on('close', function() {
+    console.log("%s is a real person that says %j", realPerson.name, realPerson.sayings)
+    process.exit();
+})
