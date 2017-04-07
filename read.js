baee243cae3ba81d20a303da8f96452983cd56b3
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-07 18:41:49 -0400

    used readFile and readdir together

--- read.js
+++ read.js
@@ -1,9 +1,16 @@
 var fs = require("fs");
+var path = require("path");
 
- fs.readFile("./lib/sayings.md","UTF-8", function(err, contents) {
-     if (err) {
-         console.log(err);
-     } 
-     console.log(contents);
- });
+fs.readdir("./lib", function(err, files) {
+    files.forEach(function(fileName) {
+        var file = path.join(__dirname, 'lib', fileName);
+        var stats = fs.statSync(file);
+        if (stats.isFile() && fileName !== ".DS_Store") {
+            fs.readFile(file, "UTF-8", function(err, contents) {
+                console.log(contents);
+            })
+        }
+    })
+});
+
 
