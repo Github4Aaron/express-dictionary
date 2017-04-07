c9fa8e2814595f2aa79663510719ce29690db430
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-07 14:56:02 -0400

    util.inherits provided inheritance, person object inherits functions of EventEmitter

--- benFranklin.js
+++ benFranklin.js
@@ -1,10 +1,22 @@
-var events = require('events');
+var EventEmitter  = require('events').EventEmitter; //chain on to require and pulls constructor function out of event module.
+var util = require('util');
 
-var emitter = new events.EventEmitter();//This is a contstructor to create an new instance of the emitter. 
+var Person = function(name) { //JS objects are functions
+    this.name = name;  //this object has a name property
+};
 
+//util.inherit allows inheritance
+util.inherits(Person, EventEmitter);//This one line means that the Person object inherits the EventEmitter
+//Person inherits functionality of event emitter
 
-emitter.on('customEvent', function(message, status) {
-    console.log(`${status}: ${message}`);
+var ben = new Person("Ben Franklin");
 
+ben.on('speak', function(said) {  //Person ben is using event function of on
+    console.log(`${this.name}: ${said}`);
 });
-emitter.emit('customEvent', "hello world", 200);
+
+ben.emit('speak', "You may delay, but time will not."); 
+
+//The EventEmitter provides us a way to create custom objects that raise custom events that can be handled asynchronously.
+
+//And because the events are handled asynchronously, it is a very important tool in node.js.
