68b7e6918c1da8936fc70b2b12883064c8971a41
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-07 14:12:10 -0400

    readline.setPrompt, prompt, on, and question functions used

--- promptPerson.js
+++ promptPerson.js
@@ -1,4 +1,4 @@
-var readline = require('readline');
+var readline = require('readline'); //RL controls objects for asking and reciving info
 
 var rl = readline.createInterface(process.stdin, process.stdout);
 var realPerson = {
@@ -7,5 +7,12 @@ var realPerson = {
 };
 
 rl.question("What is the name of a real person?", function(answer) {
-    console.log(answer);
+   
+   realPerson.name = answer;
+   rl.setPrompt(`What would ${realPerson.name} say?`); // function to ask over and over
+   rl.prompt();
+
+   rl.on('line', function(saying){
+    console.log(saying.trim());
+   });
 });
