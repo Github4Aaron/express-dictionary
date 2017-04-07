413d2624a923461b91d1c0266e0d49f9516f4208
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-07 16:39:42 -0400

    child_process.spawn function used

---
+++ alwaysTalking.js
@@ -0,0 +1,23 @@
+var sayings = [
+    "You may delay, but time will not.",
+    "Tell me and I forget. Teach me and I remember. Involve me and I learn.",
+    "It takes many good deeds to build a good reputation, and only one bad one to lose it.",
+    "Early to bed and early to rise makes a man healthy, wealthy and wise.",
+    "By failing to prepare, you are preparing to fail.",
+    "An investment in knowledge pays the best interest.",
+    "Well done is better than well said."
+];
+
+var interval = setInterval(function() { //every second randomly write to stdout
+	var i = Math.floor(Math.random() * sayings.length);
+	process.stdout.write(`${sayings[i]} \n`);
+}, 1000);
+
+process.stdin.on('data', function(data) { //listener on stdin, callback function fires
+	console.log(`STDIN Data Recieved -> ${data.toString().trim()}`);
+	clearInterval(interval);
+	process.exit();
+});
+
+//exec is used for short processes
+//spawn is used for long ongiong process
