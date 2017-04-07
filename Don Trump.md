e26e67fb69b33d3b56a766b5f3667563e3c416c6
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-07 19:03:53 -0400

    used fs.writeFileSync & fs.appendFile to create file and append saying

---
+++ Don Trump.md
@@ -0,0 +1,4 @@
+Don Trump
+=========
+
+* Drain the swamp
--- promptPerson.js
+++ promptPerson.js
@@ -1,5 +1,5 @@
 var readline = require('readline'); //RL controls objects for asking and reciving info
-
+var fs = require("fs");
 var rl = readline.createInterface(process.stdin, process.stdout);
 var realPerson = {
     name: '', // empty string
@@ -9,11 +9,18 @@ var realPerson = {
 rl.question("What is the name of a real person?", function(answer) {
    
    realPerson.name = answer;
+
+   //This will create a new markdown file with the person's name 
+   fs.writeFileSync(realPerson.name + ".md", `${realPerson.name}\n=========\n\n`);
+
    rl.setPrompt(`What would ${realPerson.name} say?`); // function to ask over and over
    rl.prompt();
 
    rl.on('line', function(saying){
        realPerson.sayings.push(saying.trim());
+
+       //This line will append the saying to their file. 
+       fs.appendFile(realPerson.name + ".md", `* ${saying.trim()}\n`);
 
        if (saying.toLowerCase().trim() === 'exit') {
            rl.close();
