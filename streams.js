c1ddd458580c9153e7b5061855624dbbb1e4d6d3
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-08 13:47:31 -0400

    used stream.on('data'), stream.once('data'), and stream.on('end')

--- streams.js
+++ streams.js
@@ -1,10 +1,30 @@
 var fs = require("fs"); //fs used to read chatlog.
 
-fs.readFile("./chat.log", "UTF-8", function(err, chatlog) { //reading chatlog file, as UTF 8, and tehn callback invoked once we have file..
-    console.log(`File read ${chatlog.length}`);
+var stream = fs.createReadStream("./chat.log", "UTF-8"); //Using Stream we don't have to wait to for entire file to load
+
+var data = ""; //data contents will go in this variable.
+
+stream.once("data", function() { //callback will fire only once
+    console.log("\n\n\n");
+    console.log("Started Reading File");
+    console.log("\n\n\n");
+});
+//listening for data events, just a small chunk
+stream.on("data", function(chunk) {
+//display length of chunks on terminal
+    process.stdout.write(`chunk: ${chunk.length} |`); // pipe separates more chunks.
+    data += chunk;
 });
 
-console.log("Reading File");
-
+stream.on("end", function() { //listener is wired up for an end event.
+    console.log("\n\n\n");
+    console.log(`Finished Reading File ${data.length}`);
+    console.log("\n\n\n");
+})
 //Stream enable us to asyncrhonously handle continuous data flows. ask.js file uses this. 
-//process.stdout/stdin use the stream interface. 
+//process.stdout/stdin use the stream interface.
+
+//File reading process happened in various stages. 
+
+//we did not have to wait to buffer the data. 
+//Readable file stream
