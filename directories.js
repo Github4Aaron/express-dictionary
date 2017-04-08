82b65d0e2a5f35a4547d759390787d09a0f6ae31
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-08 10:22:53 -0400

    fs.rmdir demo

--- directories.js
+++ directories.js
@@ -1,6 +1,22 @@
 var fs = require("fs");
 
-fs.renameSync("./assets/logs", "./logs");//1st arg is What you want to move, 2nd arg is where you want to move it. 
+// fs.renameSync("./assets/logs", "./logs");//1st arg is What you want to move, 2nd arg is where you want to move it. 
 
 
-console.log("This was a successful directory move.");
+// console.log("This was a successful directory move.");
+
+fs.rmdir("./assets",function(err) {
+    if(err) {
+        throw err;
+    }
+    console.log("Assets directory removed");
+});
+
+fs.rmdir("./logs", function(err) { //async used with callback
+   if (err) {
+       throw err;
+   }
+   console.log("logs directory removed");
+});
+
+//Cannot remove a directory that is not empty, so you must first remove files from directory.
