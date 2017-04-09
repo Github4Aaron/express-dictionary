b9cf0cd9956feaacd7d60d18cd0245a35f697452
Aaron Strouse <aaronstrouse@gmail.com>
2017-04-08 21:18:52 -0400

    http server request responding with html in template string

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 var http = require("http");
 var server = http.createServer(function(req, res) {//any call to server, will cause callback to be invoked
     //content type tells broswer what kind of text we are responding with
-    res.writeHead(200, {"content-Type": "text/plain"});//this could be changed to html
-    res.end("hello world");
+    res.writeHead(200, {"content-Type": "text/html"});//this could be changed to html
+    res.end(`
+        <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>HTML Response</title>
+                </head>
+                <body>
+                    <h1>Serving HTML Text</h1>
+                    <p>${req.url}</p>
+                    <p>${req.method}</p>
+                </body>
+            </html>
+
+    `)
 });
 
 server.listen(3000);//tell server what IP and port to listen for request
